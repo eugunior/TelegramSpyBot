d836158462122ca4a3112462cd18dcd0238e37d5
eugunior <119051392+eugunior@users.noreply.github.com>
2023-10-26 19:57:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-# you can find both params here: https://my.telegram.org/auth?to=apps
+# you can find both params here: https://my.telegram.org/apps
 API_ID = ''
 API_HASH = ''
 
 # @BotFather bot_token
 BOT_TOKEN = ''
-
 
 # delay in seconds
 delay = 1
